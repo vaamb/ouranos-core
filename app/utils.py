74f62029341512f47f7eb2e0ba5ace3d6820f58a
Valentin Ambroise <valentin.ambroise@outlook.com>
2021-02-04 18:31:52 +0100

    Geocoding location
    - Added a LRU dict object in utils.py
    
    - Added get_coordinates(city) function in utils.py. This function return a dict with latitude and longitude from the given city. It uses on geopy.geocoders.Nominatim() which is free but limited. To reduce the call numbers, the data is stored in a LRU

--- app/utils.py
+++ app/utils.py
@@ -1,6 +1,9 @@
+from collections import OrderedDict
 import os
 from pathlib import Path
 import socket
+
+import geopy
 
 from config import Config
 
@@ -10,7 +13,30 @@ if not cache_dir:
     os.mkdir(cache_dir)
 
 
-def is_connected():
+class LRU(OrderedDict):
+    # Recipe taken from python doc
+    def __init__(self, maxsize=32, *args, **kwargs):
+        self.maxsize = maxsize
+        super().__init__(*args, **kwargs)
+
+    def __getitem__(self, key):
+        value = super().__getitem__(key)
+        self.move_to_end(key)
+        return value
+
+    def __setitem__(self, key, value):
+        if key in self:
+            self.move_to_end(key)
+        super().__setitem__(key, value)
+        if len(self) > self.maxsize:
+            oldest = next(iter(self))
+            del self[oldest]
+
+
+coordinates = LRU(maxsize=16)
+
+
+def is_connected() -> bool:
     try:
         host = socket.gethostbyname(Config.TEST_CONNECTION_IP)
         s = socket.create_connection((host, 80), 2)
@@ -19,3 +45,23 @@ def is_connected():
     except Exception as ex:
         print(ex)
     return False
+
+
+def get_coordinates(city: str) -> dict:
+    """
+    Memoize and return the geocode of the given city using geopy API. The
+    memoization step allows to reduce the number of call to the Nominatim API.
+
+    :param city: str, the name of a city.
+    :return: dict with the latitude and longitude of the given city.
+    """
+    # if not memoized, look for coordinates
+    if city not in coordinates:
+        geolocator = geopy.geocoders.Nominatim(user_agent="EP-gaia")
+        location = geolocator.geocode(city)
+        coordinates[city] = {
+            "latitude": location.latitude,
+            "longitude": location.longitude
+        }
+
+    return coordinates[city]
