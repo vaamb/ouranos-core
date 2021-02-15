c12d3e0977807f4a223899319e7f612e2dbf7a25
Valentin Ambroise <valentin.ambroise@outlook.com>
2021-02-15 23:39:54 +0100

    systemData uses TTLCache
    - systemData uses TTLCache with a ttl of 2 minutes

--- app/dataspace.py
+++ app/dataspace.py
@@ -11,7 +11,7 @@ lock = Lock()
 sensorsData = TTLCache(maxsize=32, ttl=Config.ECOSYSTEM_TIMEOUT)
 # TODO: use a cache which checks db if value for today present, if so load it
 healthData = {}
-systemData = {}
+systemData = TTLCache(maxsize=2, ttl=120)
 
 
 __all__ = ("lock", "sensorsData", "healthData", "systemData")
