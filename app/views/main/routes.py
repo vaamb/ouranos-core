9b85412d5a04c2fe19c7ffbd3455ac73fc03d126
Valentin Ambroise <valentin.ambroise@outlook.com>
2021-04-09 00:47:56 +0200

    Use of API in views.main.routes

--- app/views/main/routes.py
+++ app/views/main/routes.py
@@ -1,22 +1,19 @@
-from datetime import date, datetime, time, timedelta, timezone
-import platform
+from datetime import datetime, timezone
 
 import cachetools.func
-from cachetools import cached
 from flask import abort, current_app, flash, redirect, render_template, \
     request, url_for
 from flask_login import current_user, login_required
 from flask_sqlalchemy import get_debug_queries
 
 from app import db
+from app import API
+from app.API.various import wiki
 from app.dataspace import lock, sensorsData, sensorsDataHistory
-from app.models import sensorData, Ecosystem, Hardware, Health, Service, User, \
-    Management, Warning_table
+from app.models import Hardware, Health, Service, User
 from app.services import get_manager
 from app.views.main import bp, layout
 from app.views.main.forms import EditProfileForm
-from app.utils import parse_sun_times
-from app.wiki import simpleWiki
 from config import Config
 
 
@@ -25,116 +22,15 @@ def weather_service():
     return get_manager().services["weather"]
 
 
-def sun_times_service():
-    return get_manager().services["sun_times"]
-
-
 def system_monitor():
     return get_manager().services["system_monitor"]
 
 
-@cachetools.func.ttl_cache(ttl=3)
-def time_limits() -> dict:
-    now_utc = datetime.now(timezone.utc)
-    return {
-        "ecosystems": (now_utc - timedelta(hours=36)),
-        "sensors": (now_utc - timedelta(days=2)).replace(tzinfo=None),
-        "health": (now_utc - timedelta(days=7)).replace(tzinfo=None),
-        "status": (now_utc - timedelta(seconds=Config.ECOSYSTEM_TIMEOUT)
-                   ).replace(tzinfo=None)
-    }
-
-
-@cachetools.func.ttl_cache(ttl=60)
-def get_recent_warnings():
-    time_limit = (datetime.now(timezone.utc) - timedelta(days=7))
-    return (Warning_table.query.filter(Warning_table.datetime >= time_limit)
-            .filter(Warning_table.solved == False)
-            .order_by(Warning_table.level.desc())
-            .order_by(Warning_table.row_id)
-            .all()
-            )
-
-
-def recent_ecosystems():
-    time_limit = (datetime.now(timezone.utc) - timedelta(hours=36))
-    return Ecosystem.query \
-                    .filter(Ecosystem.last_seen >= time_limit) \
-                    .all()
-
-
-def time_to_datetime(t: time) -> datetime:
-    if isinstance(t, time):
-        t = datetime.combine(date.today(), t, tzinfo=timezone.utc)
-    return t
-
-
-def on_off(value: bool) -> str:
-    if value:
-        return "on"
-    return "off"
-
-
-def get_ecosystem_ids(ecosystem_name: str) -> str:
-    ecosystem_ids = (Ecosystem.query.filter_by(name=ecosystem_name)
-                     .with_entities(Ecosystem.id,
-                                    Ecosystem.name).first_or_404())
-    return ecosystem_ids
-
-
-@cached(sensorsDataHistory, lock=lock)
-def get_sensors_data(level: str, ecosystem_uid: str, days: int = 7) -> dict:
-    time_limit = datetime.now(timezone.utc) - timedelta(days=days)
-    data = {}
-
-    measures = [
-        d.measure for d in
-        sensorData.query.join(Hardware)
-                        .filter(sensorData.ecosystem_id == ecosystem_uid)
-                        .filter(Hardware.level == level)
-                        .filter(sensorData.datetime >= time_limit)
-                        .group_by(sensorData.measure)
-                        .all()
-    ]
-
-    for measure in measures:
-        data[measure] = {}
-        sensors_id = [
-            d.sensor_id for d in
-            sensorData.query.join(Hardware)
-                            .filter(sensorData.ecosystem_id == ecosystem_uid)
-                            .filter(Hardware.level == level)
-                            .filter(sensorData.measure == measure)
-                            .group_by(sensorData.sensor_id)
-                            .all()
-        ]
-
-        for sensor_id in sensors_id:
-            sensor_name = Hardware.query.filter_by(id=sensor_id).first().name
-            _data = (sensorData.query.join(Hardware)
-                     .filter(sensorData.ecosystem_id == ecosystem_uid)
-                     .filter(Hardware.level == level)
-                     .filter(sensorData.measure == measure)
-                     .filter(sensorData.sensor_id == sensor_id)
-                     .filter(sensorData.datetime >= time_limit)
-                     .with_entities(sensorData.datetime, sensorData.value).all())
-            data[measure][sensor_id] = {"name": sensor_name,
-                                        "values": _data}
-    return {ecosystem_uid: data}
-
-
-def get_light_data(ecosystem_query):
-    return {
-        e.id: {
-            "status": e.light.one().status and e.manages(Management["light"]),
-            "mode": e.light.one().mode,
-            "method": e.light.one().method,
-            "morning_start": time_to_datetime(e.light.one().morning_start),
-            "morning_end": time_to_datetime(e.light.one().morning_end),
-            "evening_start": time_to_datetime(e.light.one().evening_start),
-            "evening_end": time_to_datetime(e.light.one().evening_end),
-        } for e in ecosystem_query
-    }
+def get_ecosystem_ids(ecosystem, time_limit=None) -> str:
+    ids = API.ecosystems.get_ecosystem_ids(ecosystem=ecosystem,
+                                           session=db.session,
+                                           time_limit=time_limit)
+    return ids or abort(404)
 
 
 @bp.after_app_request
@@ -158,72 +54,28 @@ def before_request():
 @bp.app_context_processor
 @cachetools.func.ttl_cache(ttl=60)
 def menu_info():
-    limits = time_limits()
-    # TODO: move to API
-    ecosystems = {
-        e.id: {
-            "name": e.name,
-            "status": on_off(e.status) if e.last_seen >= limits["status"] else "disconnected",
-            "webcam": e.manages(Management["webcam"]),
-            "lighting": e.manages(Management["light"]),
-            "health": True if (
-                Health.query.filter_by(ecosystem_id=e.id)
-                      .filter(Health.datetime >= limits["health"])
-                      .first()) else False,
-            "env_sensors": True if (
-                Hardware.query.filter_by(ecosystem_id=e.id)
-                        .filter_by(type="sensor", level="environment")
-                        .filter(Hardware.last_log >= limits["sensors"])
-                        .first()) else False,
-            "plant_sensors": True if (
-                Hardware.query.filter_by(ecosystem_id=e.id)
-                        .filter_by(type="sensor", level="plants")
-                        .filter(Hardware.last_log >= limits["sensors"])
-                        .first()) else False,
-            "lights": True if e.hardware.filter_by(type="light").first() else False,
-            "switches": True if e.hardware.filter_by(type="light").first()
-                            and e.manages(Management["light"])
-                            and e.id in sensorsData else False,
-        }
-        for e in recent_ecosystems()
-    }
+    ecosystems_qo = API.ecosystems.get_recent_ecosystems_query_obj(session=db.session)
+    ecosystems_info = API.ecosystems.get_ecosystems_info(
+        ecosystems_qo, session=db.session)
+    summarized_ecosystems_info = API.ecosystems.summarize_ecosystems_info(
+        ecosystems_info, session=db.session)
+    dropdowns = API.ecosystems.get_app_functionalities(
+        summarized_ecosystems_info)
 
-    # list comprehension for menu dropdown before passing to jinja
-    dropdowns = {
-        "weather": True if weather_service().get_data() else False,
-        "webcam": True if (True in [ecosystems[ecosystem]["webcam"]
-                                    for ecosystem in ecosystems] and
-                           "webcam" in [s.name
-                                        for s in Service.query
-                                                        .filter_by(status=1)
-                                                        .all()]
-                           ) else False,
-        "lighting": True if True in [ecosystems[ecosystem]["lighting"]
-                                     for ecosystem in ecosystems] else False,
-        "health": True if True in [ecosystems[ecosystem]["health"]
-                                   for ecosystem in ecosystems] else False,
-        "env_sensors": True if True in [ecosystems[ecosystem]["env_sensors"]
-                                        for ecosystem in
-                                        ecosystems] else False,
-        "plant_sensors": True if True in [
-            ecosystems[ecosystem]["plant_sensors"]
-            for ecosystem in ecosystems] else False,
-        "switches": True if True in [ecosystems[ecosystem]["switches"]
-                                     for ecosystem in ecosystems] else False,
-    }
-
-    plants = []
+    plant_articles = [article for article
+                      in sorted(wiki.articles_available["plants_care"])]
+    plant_articles.insert(0, plant_articles.pop(plant_articles.index("general")))
 
     warnings = []
-
     if current_user.is_authenticated:
-        warnings = get_recent_warnings()
+        warnings = API.warnings.get_recent_warnings(db.session)
 
-    return {"ecosystems": ecosystems,
-            "dropdowns": dropdowns,
-            "plants_in_wiki": plants,
-            "warnings": warnings,
-            }
+    return {
+        "ecosystems": ecosystems_info,
+        "dropdowns": dropdowns,
+        "plants_in_wiki": plant_articles,
+        "warnings": warnings,
+    }
 
 
 @bp.route("/")
@@ -233,52 +85,24 @@ def to_home():
 
 @bp.route("/home")
 def home():
-    light_data = get_light_data(recent_ecosystems())
-    sun_times = {}
-    try:
-        up = {
-            "sunrise": parse_sun_times(
-                sun_times_service().get_data()["sunrise"]),
-            "sunset": parse_sun_times(
-                sun_times_service().get_data()["sunset"]),
-        }
-        sun_times.update(up)
-    except Exception as e:
-        print(e)
-
-    measures = {}
-    limits = time_limits()
-    for ecosystem_id in sensorsData:
-        measures[ecosystem_id] = {"environment": {}, "plants": {}}
-        for level in ("environment", ):
-            measures_lists = [d.measure for d in
-                              Hardware.query
-                                  .filter(Hardware.ecosystem_id == ecosystem_id)
-                                  .filter(Hardware.level == level)
-                                  .filter(Hardware.type == "sensor")
-                                  .filter(Hardware.last_log >= limits["sensors"])
-                              ]
-            for measures_list in measures_lists:
-                for measure in measures_list:
-                    measures[ecosystem_id][level][measure.name] = measure.unit
-
+    current_weather = API.weather.get_current_weather()
+    sun_times = API.weather.get_suntimes_data()
+    ecosystems_qo = API.ecosystems.get_recent_ecosystems_query_obj(session=db.session)
+    light_data = API.ecosystems.get_light_info(ecosystems_qo)
+    system_data = system_monitor().system_data
     return render_template("main/home.html", title="Home",
-                           weather_data=weather_service().get_data(),
+                           weather_data=current_weather,
                            light_data=light_data,
                            sun_times=sun_times,
-                           platform=platform.system(),
-                           system_data=system_monitor().system_data,
-                           current_data=sensorsData,
-                           measures=measures,
+                           system_data=system_data,
+                           current_data={**sensorsData},
                            parameters=layout.parameters,
                            )
 
 
 @bp.route("/weather")
 def weather():
-    weather_data = weather_service().get_data()
-    if not weather_data:
-        abort(404)
+    weather_data = weather_service().get_data() or abort(404)
     return render_template("main/weather.html", title="Weather",
                            home_city=Config.HOME_CITY,
                            weather_data=weather_data,
@@ -288,14 +112,23 @@ def weather():
 @bp.route("/sensors/<level>/<ecosystem_name>")
 def sensors(level: str, ecosystem_name: str):
     # TODO: send the sensor model info
-    ecosystem_ids = get_ecosystem_ids(ecosystem_name=ecosystem_name)
-    data = get_sensors_data(ecosystem_uid=ecosystem_ids[0], level=level,
-                            days=7)
-    current_data = sensorsData
+    ecosystem_ids = get_ecosystem_ids(ecosystem=ecosystem_name,
+                                      time_limit=API.utils.time_limits()[
+                                          "sensors"]
+                                      )
+
+    ecosystem_qo = API.ecosystems.get_ecosystem_query_obj(
+        ecosystem_name, session=db.session
+    )
+    historic_sensors_data = API.ecosystems.get_historic_sensors_data(
+        ecosystem_qo, session=db.session, level=(level, ))
+    current_sensors_data = sensorsData
     title = f"{ecosystem_ids[1]} {level} data"
     return render_template("main/sensors.html", title=title,
-                           ecosystem_ids=ecosystem_ids, level=level,
-                           data=data, current_data=current_data,
+                           ecosystem_ids=ecosystem_ids,
+                           level=level,
+                           current_data=current_sensors_data,
+                           data=historic_sensors_data,
                            parameters=layout.parameters,
                            )
 
@@ -303,10 +136,13 @@ def sensors(level: str, ecosystem_name: str):
 @bp.route("/health")
 @bp.route("/health/<ecosystem_name>")
 def health(ecosystem_name: str):
-    ecosystem_ids = get_ecosystem_ids(ecosystem_name=ecosystem_name)
+    ecosystem_ids = get_ecosystem_ids(ecosystem=ecosystem_name,
+                                      time_limit=API.utils.time_limits()[
+                                          "health"]
+                                      )
     data = (Health.query.filter(Health.ecosystem_id == ecosystem_ids[0])
             .filter(
-        Health.datetime >= (datetime.utcnow() - timedelta(days=31)))
+        Health.datetime >= API.utils.time_limits()["health"])
             .with_entities(Health.datetime, Health.health_index, Health.green,
                            Health.necrosis)
             .all())
@@ -321,17 +157,11 @@ def health(ecosystem_name: str):
 @bp.route("/switches")
 @bp.route("/switches/<ecosystem_name>")
 def switches(ecosystem_name: str):
-    if not ecosystem_name:
-        abort(404)
-    ecosystem_ids = get_ecosystem_ids(ecosystem_name=ecosystem_name)
-
-    if ecosystem_ids[0] not in sensorsData:
-        abort(404)
+    ecosystem_ids = get_ecosystem_ids(
+        ecosystem=ecosystem_name, time_limit="connected")
     title = f"{ecosystem_ids[1]} switches control"
-
-    e = Ecosystem.query.filter(Ecosystem.name == ecosystem_name)\
-                       .one()
-    light_data = get_light_data((e, ))
+    ecosystems_qo = API.ecosystems.get_ecosystem_query_obj(session=db.session)
+    light_data = API.ecosystems.get_light_info(ecosystems_qo)
 
     return render_template("main/switches.html", title=title,
                            ecosystem_ids=ecosystem_ids,
@@ -342,7 +172,7 @@ def switches(ecosystem_name: str):
 @bp.route("/settings")
 @bp.route("/settings/<ecosystem_name>")
 def settings(ecosystem_name: str):
-    ecosystem_ids = get_ecosystem_ids(ecosystem_name=ecosystem_name)
+    ecosystem_ids = get_ecosystem_ids(ecosystem=ecosystem_name)
     title = f"{ecosystem_ids[1]} settings"
 
     # TODO: first generate a ecosystem query, then use it
@@ -380,20 +210,18 @@ def settings(ecosystem_name: str):
 @login_required
 def care(species: str = "general"):
     title = f"{species.capitalize()} care"
+    article = wiki.get_article("plants_care", species)
     return render_template("main/care.html", title=title,
-                           metadata=simpleWiki().get_article("plants_care",
-                                                             species,
-                                                             data="metadata"),
-                           content=simpleWiki().get_article("plants_care",
-                                                            species),
+                           article=article,
                            )
 
 
 @bp.route("/warnings")
 @login_required
 def warnings_list():
+    warnings = API.warnings.get_recent_warnings(db.session)
     return render_template("main/warning.html", title="Warnings",
-                           warnings=get_recent_warnings()
+                           warnings=warnings
                            )
 
 
