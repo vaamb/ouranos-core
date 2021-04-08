6bd8b26358f43613aaed34139464e747c5e52329
Valentin Ambroise <valentin.ambroise@outlook.com>
2021-04-09 00:43:03 +0200

    Refresh simpleWiki

--- app/wiki/__init__.py
+++ app/wiki/__init__.py
@@ -1,59 +1,76 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-import os
+from collections import namedtuple
+from datetime import datetime, timezone
 import pathlib
+
+import cachetools.func
 import markdown
 import markdown.extensions.extra
-from datetime import datetime
 
-class simpleWiki():
+
+article_tuple = namedtuple("article_tuple", ["metadata", "html"])
+
+
+class simpleWiki:
     def __init__(self):
         self.script_path = pathlib.Path(__file__).parent.absolute()
         self.wiki_path = pathlib.Path(__file__).parent.absolute()
+        self.md_parser = markdown.Markdown(extensions=["extra", "meta"])
 
+    @property
     def categories_available(self):
-        categories = os.listdir(self.wiki_path)
-        categories.remove("template")
+        categories = {i.name: i for i in self.wiki_path.iterdir()
+                      if i.is_dir() and "__" not in i.name}
+        categories.pop("template")
         return categories
 
-    def articles_available(self, category = ""):
-        path = os.path.join(self.wiki_path, category)
-        articles = []
-        for dirpath, dirnames, filenames in os.walk(path):
-            for file in filenames:        
-                if 'main.md' in file:
-                    articles.append(os.path.relpath(dirpath, path))
-        articles = [a.replace('\\', '/') for a in articles] #needed for windows path
-        if articles != []:
-            return articles
-        else:
-            return "It looks like this category is either empty or does not exist. Check the existing categories by using 'categories_available()'"
+    @property
+    @cachetools.func.ttl_cache(maxsize=1, ttl=60)
+    def articles_available(self):
+        categories = [key for key in self.categories_available]
+        articles = {}
+        for category in categories:
+            path = self.wiki_path/category
+            articles[category] = {
+                d.name: d/"main.md" for d in path.iterdir()
+                if d.is_dir()
+                and "main.md" in [f.name for f in d.iterdir()]
+            }
+        return articles
 
-    def get_article(self, *args, data = "html"):
-        path = self.wiki_path
-        for subcaterory in args:
-            path = os.path.join(path, subcaterory)
-        article_path = os.path.join(path, "main.md")
+    def get_article(self, category, article):
         try:
-            md = markdown.Markdown(extensions = ["extra", "meta"])
-            f = open(article_path, "r", encoding = 'utf-8')
-            html = md.convert(f.read())
-            f.close()
-            if data == "html":
-                return html
-            elif data == "metadata":
-                return md.Meta
-        except IOError:
-            return("It looks like this article does not exist. Check the existing categories by using 'articles_available()")
-                
+            article_path = self.articles_available[
+                category.lower()][article.lower()]
+        except KeyError:
+            print("This article doesn't exists")
+            return
+        with open(article_path) as article_file:
+            html = self.md_parser.convert(article_file.read())
+        return article_tuple(self.md_parser.Meta, html)
+
     def create_template(self):
         pass
-    
-    def create_article(self, category = "", article = "article", title = "Title", author = "The Author", date = datetime.now(), summary = "Summary"):
+
+    def create_article(self,
+                       body_text,
+                       category,
+                       subject,
+                       title,
+                       author="The Author",
+                       date=datetime.now(timezone.utc),
+                       summary="Summary"):
         pass
-        #think about the metadata
+        # TODO: think about the metadata
     
-    def modify_article(self, *args, author = "The Author"):
+    def modify_article(self):
         pass
-        #add the author in meta data
+
+
+_wiki = None
+
+
+def get_wiki():
+    global _wiki
+    if not _wiki:
+        _wiki = simpleWiki()
+    return _wiki
