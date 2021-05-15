4d3952d21c95ee145685f95ff566f2990f5bab7e
Valentin Ambroise <valentin.ambroise@outlook.com>
2021-05-15 20:53:48 +0200

    Use of argparse

--- gaiaWeb.py
+++ gaiaWeb.py
@@ -7,6 +7,7 @@ import eventlet
 
 eventlet.monkey_patch()
 
+import argparse
 import logging
 import signal
 import sys
@@ -26,6 +27,23 @@ profiles_available = [profile for profile in config_profile]
 default_profile = "development"
 
 
+def get_config(profile):
+    if profile.lower() in ("dev", "development"):
+        return config_profile["development"]
+    elif profile.lower() in ("test", "testing"):
+        return config_profile["testing"]
+    elif profile.lower() in ("prod", "production"):
+        return config_profile["production"]
+    else:
+        print(f"{profile} is not a valid profile. Valid profiles are "
+              f"{humanize_list(profiles_available)}.")
+
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-p", "--profile", default=default_profile)
+
+
 def graceful_exit(*args):
     services.exit_gracefully()
     print("gaiaWeb has been closed")
@@ -38,15 +56,8 @@ signal.signal(signal.SIGTERM, graceful_exit)
 
 if __name__ == "__main__":
     try:
-        profile = default_profile
-        if len(sys.argv) > 1:
-            profile = sys.argv[1]
-        try:
-            config_class = config_profile[profile]
-        except KeyError:
-            raise ValueError(f"Profile {profile} not available, Please choose "
-                             f"from {humanize_list(profiles_available)}")
-
+        args = parser.parse_args()
+        config_class = get_config(args.profile)
         configure_logging(config_class)
         app_name = config_class.APP_NAME
         logger = logging.getLogger(app_name)
