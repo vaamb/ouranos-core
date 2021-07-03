19c051767b098b48e7a23616bcbf35b906bd63d3
Valentin Ambroise <valentin.ambroise@outlook.com>
2021-07-03 23:16:42 +0200

    Event dispatcher using pubsub
    - Event dispatcher using a pubsub system. Two classes are available: PubSubDispatcher (based on StupidPubSub) and RedisDispatcher (based on Redis PUBSUB). A name is required for class init. Upon initialisation, the dispatcher automatically subscribe to the channel corresponding to "to_<dispatcher_name>". Dispatcher can send event to another dispatcher using its name, an event and some additional data in the form of a dict. Events with callbacks can be registered using the decorator @dispatcher.on. To start automatically dispatching events, use "dispatcher.start()". Upon receiving an event, the dispatcher will automatically trigger the callback linked to the event, or a fallback function if set

---
+++ dataspace/dispatcher.py
@@ -0,0 +1,135 @@
+from threading import Event, Thread
+import time
+
+from utils import jsonWrapper as json
+
+
+_thread = None
+
+
+class BaseDispatcher:
+    def __init__(self, name):
+        self.name = name
+        self.handlers = {}
+        self._fallback = None
+        self.threads = {}
+        self._subscribe(f"to_{name}")
+        self._running = Event()
+
+    def _subscribe(self, channel):
+        raise NotImplementedError(
+            "This method needs to be implemented in a subclass"
+        )
+
+    def _publish(self, channel: str, payload: dict):
+        raise NotImplementedError(
+            "This method needs to be implemented in a subclass"
+        )
+
+    def on(self, event: str, handler=None):
+        def set_handler(handler):
+            self.handlers[event] = handler
+            return handler
+
+        if handler is None:
+            return set_handler
+        set_handler(handler)
+
+    @property
+    def fallback(self):
+        return self._fallback
+
+    @fallback.setter
+    def fallback(self, fct):
+        self._fallback = fct
+
+    def emit(self, namespace: str, event: str, data: dict = None):
+        payload = {"event": event}
+        if data:
+            payload.update({"args": data})
+        self._publish(f"to_{namespace}", payload)
+
+    def main_loop(self):
+        while self._running.is_set():
+            message = self._parse_payload(self._get_message())
+            if message:
+                event = message["event"]
+                data = message.get("data", {})
+                self._trigger_event(event, **data)
+            time.sleep(0.01)
+
+    def _get_message(self) -> dict:
+        raise NotImplementedError(
+            "This method needs to be implemented in a subclass"
+        )
+
+    def _parse_payload(self, payload: dict) -> dict:
+        raise NotImplementedError(
+            "This method needs to be implemented in a subclass"
+        )
+
+    def _trigger_event(self, event: str, *args, **kwargs):
+        if event in self.handlers:
+            return self.handlers[event](*args, **kwargs)
+        else:
+            if self._fallback:
+                return self._fallback(*args, **kwargs)
+
+    def start_background_task(self, target, *args):
+        """Override to use another threading method"""
+        t = Thread(target=target, args=args)
+        t.start()
+        self.threads[target.__name__] = target
+
+    def start(self):
+        if self._running.is_set():
+            return
+        self._running.set()
+        self.start_background_task(target=self.main_loop)
+
+    def stop(self):
+        self._running.clear()
+        for thread in self.threads:
+            thread.join()
+
+
+class PubSubDispatcher(BaseDispatcher):
+    def __init__(self, name, pubsub):
+        self.pubsub = pubsub
+        super(PubSubDispatcher, self).__init__(name)
+    
+    def _subscribe(self, channel):
+        self.pubsub.subscribe(channel)
+    
+    def _publish(self, channel, payload):
+        return self.pubsub.publish(channel, payload)
+    
+    def _get_message(self):
+        return self.pubsub.get_message()
+    
+    def _parse_payload(self, payload):
+        return payload
+
+
+class RedisDispatcher(BaseDispatcher):
+    def __init__(self, name, redis_client):
+        self.redis = redis_client
+        self.pubsub = redis_client.pubsub()
+        super(RedisDispatcher, self).__init__(name)
+
+    def _subscribe(self, channel):
+        self.pubsub.subscribe(channel)
+
+    def _publish(self, channel, payload):
+        message = json.dumps(payload)
+        return self.redis.publish(channel, message)
+
+    def _get_message(self):
+        return self.pubsub.get_message(ignore_subscribe_messages=True) or {}
+
+    def _parse_payload(self, payload):
+        if payload:
+            message = payload["data"].decode("utf-8")
+            message = json.loads(message)
+            return message
+        return {}
