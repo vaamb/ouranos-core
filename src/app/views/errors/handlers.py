18a147196d42be56b820404f998dd02e007acac1
Valentin Ambroise <valentin.ambroise@outlook.com>
2021-10-03 23:57:02 +0200

    Better error handler

--- src/app/views/errors/handlers.py
+++ src/app/views/errors/handlers.py
@@ -1,17 +1,26 @@
-from flask import render_template
+from flask import jsonify, render_template, request
 
 from src.app import db
 from src.app.views.errors import bp
 
 
-@bp.app_errorhandler(403)
+@bp.app_errorhandler(401)
 def forbidden(e):
-    return render_template('errors/403.html', title="Error 403",
-                           description=e.description), 403
+    return render_template('errors/401.html', title="Error 401",
+                           description=e.description), 401
 
 
 @bp.app_errorhandler(404)
 def page_not_found(e):
+    if request.path.startswith('/api/'):
+        return jsonify({"error": "Not found"}), 404
+    return render_template('errors/404.html', title="Error 404"), 404
+
+
+@bp.app_errorhandler(405)
+def method_not_allowed(e):
+    if request.path.startswith('/api/'):
+        return jsonify({"error": "Method not allowed"}), 405
     return render_template('errors/404.html', title="Error 404"), 404
 
 
