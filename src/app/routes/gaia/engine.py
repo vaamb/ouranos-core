24d368174147814a212d5b0d79bc4b6c8fdb8e6e
Valentin Ambroise <valentin.ambroise@outlook.com>
2022-08-23 13:56:10 +0200

    Better name coherence between engine routes and functions

--- src/app/routes/gaia/engine.py
+++ src/app/routes/gaia/engine.py
@@ -7,7 +7,7 @@ from src.app.dependencies import get_session
 
 
 @router.get("/engine")
-async def get_engine(uid: str = "all", session=Depends(get_session)):
+async def get_engines(uid: list = ["all"], session=Depends(get_session)):
     engines = api.gaia.get_engines(session, uid)
     response = [api.gaia.get_engine_info(
         session, engine
@@ -16,7 +16,7 @@ async def get_engine(uid: str = "all", session=Depends(get_session)):
 
 
 @router.get("/engine/u/<uid>")
-async def get_unique_engine(uid: str, session=Depends(get_session)):
+async def get_engine(uid: str, session=Depends(get_session)):
     # TODO: make sure "all" is not used
     if uid == "all":
         return {"error": "Engine not found"}, 404
@@ -28,7 +28,7 @@ async def get_unique_engine(uid: str, session=Depends(get_session)):
 
 
 @router.put("/engine/u/<uid>", dependencies=[Depends(is_admin)])
-async def put_unique_engine(
+async def put_engine(
         uid: str,
         session=Depends(get_session),
 ):
@@ -36,7 +36,7 @@ async def put_unique_engine(
 
 
 @router.delete("/engine/u/<uid>", dependencies=[Depends(is_admin)])
-async def delete_unique_engine(
+async def delete_engine(
         uid: str,
         session=Depends(get_session),
 ):
