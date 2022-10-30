a1f4797c5ecd2b12188350bcf407b3578b2bb815
Valentin Ambroise <valentin.ambroise@outlook.com>
2022-10-30 21:37:20 +0100

    Fix run.sh

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 cd $DIR
 
-if pgrep -x "ouranos" > /dev/nul; then
+if pgrep -x "ouranos" > /dev/null; then
   pkill -15 "ouranos"
 fi
 
