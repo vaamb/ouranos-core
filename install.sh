eac2419b53a5666ecb203336e041a23b3379f865
Valentin Ambroise <valentin.ambroise@outlook.com>
2022-11-06 00:21:12 +0100

    Add an installer

---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo "Installing Ouranos"
+
+# Create ouranos
+mkdir ouranos; cd ouranos
+export OURANOS_DIR=$PWD
+
+python3 -m venv venv
+source venv/bin/activate
+
+# Get Ouranos and install the package
+mkdir bin; cd bin
+git clone --branch stable https://gitlab.com/gaia/ouranos.git ouranos_core; cd ouranos_core
+pip install -r requirements.txt
+pip install -e .
+
+# Make Ouranos cli and utility scripts easily available
+cp update.sh $OURANOS_DIR/update.sh
+
+ouranos() {
+  source $OURANOS_DIR/venv/bin/activate
+  python -m ouranos "$@"
+  deactivate
+}
+
+export -f ouranos
+
+echo "Ouranos installed. To run it, either use \`ouranos\` or \`python -m ouranos\` within your venv"
