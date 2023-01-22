e72a64b3e12ccf297dbf2844cb6e4095d970ad99
Valentin Ambroise <valentin.ambroise@outlook.com>
2023-01-22 18:28:00 +0100

    Allow cache to dynamically get ttl info

--- src/ouranos/core/cache.py
+++ src/ouranos/core/cache.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from collections.abc import MutableMapping
-from enum import Enum
+from enum import IntEnum
 import logging
 
 from cachetools import Cache, TTLCache
@@ -10,26 +10,15 @@ from .redis_cache import RedisCache, RedisTTLCache
 from ouranos import current_app
 
 
-class SERVER_STATUS(Enum):
+class SERVER_STATUS(IntEnum):
     NOT_CONNECTED = 0
     CONNECTED = 1
     UNKNOWN = 2
 
 
-_state: dict = {
-    "server_status": SERVER_STATUS.UNKNOWN
-}
-
-
-_CACHE_TTL_INFO: dict[str, int] = {
-    # TODO: fix this
-    "sensors_data": 90,  # current_app.config["ECOSYSTEM_TIMEOUT"],
-    "system_data": 90,
-    "weather_data": 0,
-    "sun_times_data": 0,
-}
-
+_state: dict = {"server_status": SERVER_STATUS.UNKNOWN}
 _store: dict[str, MutableMapping] = {}
+
 
 logger: logging.Logger = logging.getLogger("ouranos.cache_factory")
 
@@ -38,6 +27,13 @@ def _create_cache(
         cache_name: str,
         config: dict,
 ) -> MutableMapping:
+    CACHE_TTL_INFO: dict[str, int] = {
+        "sensors_data": current_app.config["ECOSYSTEM_TIMEOUT"],
+        "system_data": 90,
+        "weather_data": 0,
+        "sun_times_data": 0,
+    }
+
     url = config.get("CACHING_SERVER_URL", "")
     if url.startswith("redis"):
         try:
@@ -61,14 +57,14 @@ def _create_cache(
                     )
                     _state["server_status"] = SERVER_STATUS.NOT_CONNECTED
             if _state["server_status"] == SERVER_STATUS.CONNECTED:
-                if _CACHE_TTL_INFO[cache_name] > 0:
+                if CACHE_TTL_INFO[cache_name] > 0:
                     return RedisTTLCache(
-                        cache_name, _CACHE_TTL_INFO[cache_name], _redis
+                        cache_name, CACHE_TTL_INFO[cache_name], _redis
                     )
                 else:
                     return RedisCache(cache_name, _redis)
-    elif _CACHE_TTL_INFO[cache_name] > 0:
-        return TTLCache(maxsize=32, ttl=_CACHE_TTL_INFO[cache_name])
+    elif CACHE_TTL_INFO[cache_name] > 0:
+        return TTLCache(maxsize=32, ttl=CACHE_TTL_INFO[cache_name])
     return Cache(maxsize=32)
 
 
