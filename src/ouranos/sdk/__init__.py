4b82d18ec9837a6fb4228ee57601286f581e70eb
Valentin Ambroise <valentin.ambroise@outlook.com>
2023-01-28 15:47:50 +0100

    Add a link for DispatcherFactory in sdk

--- src/ouranos/sdk/__init__.py
+++ src/ouranos/sdk/__init__.py
@@ -1,4 +1,4 @@
-from ouranos.core.utils import json
+from ouranos.core.utils import json, DispatcherFactory
 from ouranos.sdk import api
 from ouranos.sdk.functionality import Functionality, run_functionality_forever
 from ouranos.sdk.plugin import Plugin, Route
