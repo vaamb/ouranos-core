97a628d65043ab9e143dfeaf96c4410cd51f91ab
Valentin Ambroise <valentin.ambroise@outlook.com>
2023-02-01 01:24:17 +0100

    Update system models to new SQLAlchemy styles
    - Rename datetime fields to timestamp to avoid conflicts with python types

--- src/ouranos/core/database/models/system.py
+++ src/ouranos/core/database/models/system.py
@@ -1,4 +1,8 @@
+from datetime import datetime
+
 import sqlalchemy as sa
+from sqlalchemy.orm import Mapped, mapped_column
+
 
 from ouranos import db
 
@@ -12,12 +16,13 @@ base = db.Model
 class SystemHistory(base):
     __tablename__ = "system_history"
     __bind_key__ = "system"
-    id = sa.Column(sa.Integer, primary_key=True)
-    datetime = sa.Column(sa.DateTime, nullable=False)
-    CPU_used = sa.Column(sa.Float(precision=1))
-    CPU_temp = sa.Column(sa.Float(precision=1))
-    RAM_total = sa.Column(sa.Float(precision=2))
-    RAM_used = sa.Column(sa.Float(precision=2))
-    RAM_process = sa.Column(sa.Float(precision=2))
-    DISK_total = sa.Column(sa.Float(precision=2))
-    DISK_used = sa.Column(sa.Float(precision=2))
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    timestamp: Mapped[datetime] = mapped_column(nullable=False)
+    CPU_used: Mapped[float] = mapped_column(sa.Float(precision=1))
+    CPU_temp: Mapped[float] = mapped_column(sa.Float(precision=1))
+    RAM_total: Mapped[float] = mapped_column(sa.Float(precision=2))
+    RAM_used: Mapped[float] = mapped_column(sa.Float(precision=2))
+    RAM_process: Mapped[float] = mapped_column(sa.Float(precision=2))
+    DISK_total: Mapped[float] = mapped_column(sa.Float(precision=2))
+    DISK_used: Mapped[float] = mapped_column(sa.Float(precision=2))
