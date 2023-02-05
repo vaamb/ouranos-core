91c2b9a005fbd05f88ff697d515d0d04925e0f5a
Valentin Ambroise <valentin.ambroise@outlook.com>
2023-02-05 01:27:14 +0100

    Fix sqlalchemy_to_pydantic default value

--- src/ouranos/core/validate/utils.py
+++ src/ouranos/core/validate/utils.py
@@ -8,8 +8,9 @@ from sqlalchemy.orm import Mapper
 
 
 def sqlalchemy_to_pydantic(
-        db_model, exclude: list = []
+        db_model, exclude: list | None = None
 ):
+    exclude: list = exclude or []
     fields: dict[str, tuple[Type, Any | None]] = {}
     mapper: Mapper = inspect(db_model)
     for column in mapper.columns._all_columns:
@@ -18,8 +19,10 @@ def sqlalchemy_to_pydantic(
         if name in exclude:
             continue
         python_type = column.type.python_type
-        default = column.default
+        default = None
         if column.default is None and not column.nullable:
             default = ...
+        elif column.default is not None:
+            default = column.default.arg
         fields[name] = (python_type, default)
     return create_model(db_model.__name__, **fields)
