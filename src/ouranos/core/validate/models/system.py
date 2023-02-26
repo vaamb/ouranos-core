57f99f47ba1262f8b3ad2eb00f7caffbb0db8016
Valentin Ambroise <valentin.ambroise@outlook.com>
2023-02-26 15:56:02 +0100

    Add start_time route for server

--- src/ouranos/core/validate/models/system.py
+++ src/ouranos/core/validate/models/system.py
@@ -1,4 +1,4 @@
 from ouranos.core.database.models import SystemHistory
 from ouranos.core.validate.utils import sqlalchemy_to_pydantic
 
-system_record = sqlalchemy_to_pydantic(SystemHistory)
+system_record = sqlalchemy_to_pydantic(SystemHistory, exclude=["id"])
--- src/ouranos/web_server/routes/system.py
+++ src/ouranos/web_server/routes/system.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from ouranos.core import validate
+from ouranos.core.config import consts
 from ouranos.sdk import api
 from ouranos.sdk.api.utils import timeWindow
 from ouranos.web_server.auth import is_admin
@@ -17,13 +18,19 @@ router = APIRouter(
 
 
 @router.get(
+    "/start_time",
+    dependencies=[Depends(is_admin)],
+)
+async def get_current_system_data() -> int:
+    return consts.START_TIME
+
+@router.get(
     "/current_data",
     response_model=validate.system.system_record,
     dependencies=[Depends(is_admin)],
 )
 async def get_current_system_data() -> dict:
-    response = api.system.get_current_data()
-    return response
+    return api.system.get_current_data()
 
 
 @router.get(
