ab6204b1d0606e425a8fdd18f2767d5b83b5e429
Valentin Ambroise <valentin.ambroise@outlook.com>
2023-06-01 20:27:38 +0200

    Better handling of HTTP 204 responses

--- src/ouranos/web_server/routes/utils.py
+++ src/ouranos/web_server/routes/utils.py
@@ -1,13 +1,6 @@
 from fastapi import HTTPException, status
 
 
-def empty_result(result):
-    raise HTTPException(
-        status_code=status.HTTP_204_NO_CONTENT,
-        detail="No content found"
-    )
-
-
 def assert_single_uid(uid: str, name: str = "uid"):
     if len(uid.split(",")) > 1:
         raise HTTPException(
--- src/ouranos/web_server/routes/weather.py
+++ src/ouranos/web_server/routes/weather.py
@@ -1,9 +1,8 @@
 from __future__ import annotations
 
-from fastapi import APIRouter, Query
+from fastapi import APIRouter, Query, Response
 
 from ouranos.core.cache import SunTimesCache, WeatherCache
-from ouranos.web_server.routes.utils import empty_result
 from ouranos.web_server.validate.response.weather import (
     CurrentWeatherResponse, DailyWeatherResponse, HourlyWeatherResponse, SunTimesResponse)
 
@@ -19,11 +18,11 @@ router = APIRouter(
 
 
 @router.get("/sun_times", response_model=SunTimesResponse)
-async def get_sun_times() -> dict:
+async def get_sun_times():
     response = SunTimesCache.get()
     if response:
         return response
-    return empty_result(response)
+    return Response(status_code=204)
 
 
 @router.get(path="/forecast")
@@ -33,7 +32,7 @@ async def get_forecast(
             description="Period to exclude from the forecast to choose from "
                         "'currently', 'hourly' and 'daily'"
         )
-) -> dict:
+):
     response = {}
     exclude = exclude or []
     if "currently" not in exclude:
@@ -56,28 +55,28 @@ async def get_forecast(
             })
     if response:
         return response
-    return empty_result(response)
+    return Response(status_code=204)
 
 
 @router.get("/forecast/currently", response_model=CurrentWeatherResponse)
-async def get_current_forecast() -> dict:
+async def get_current_forecast():
     response = WeatherCache.get_currently()
     if response:
         return response
-    return empty_result(response)
+    return Response(status_code=204)
 
 
 @router.get("/forecast/hourly", response_model=list[HourlyWeatherResponse])
-async def get_current_forecast() -> dict:
+async def get_current_forecast():
     response = WeatherCache.get_hourly()
     if response:
         return response
-    return empty_result(response)
+    return Response(status_code=204)
 
 
 @router.get("/forecast/daily", response_model=list[DailyWeatherResponse])
-async def get_current_forecast() -> dict:
+async def get_current_forecast():
     response = WeatherCache.get_daily()
     if response:
         return response
-    return empty_result(response)
+    return Response(status_code=204)
