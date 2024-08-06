601355dd43ed4cae4255a373b8f55c67d730b8a3
Valentin Ambroise <valentin.ambroise@outlook.com>
2024-08-07 00:44:47 +0200

    Add naming convention for unnamed indexes and constrains

--- src/ouranos/core/database/base.py
+++ src/ouranos/core/database/base.py
@@ -1,6 +1,7 @@
 from typing import Any
 
 from sqlalchemy.orm import DeclarativeMeta
+from sqlalchemy.sql.schema import MetaData
 
 from sqlalchemy_wrapper.base import BindMetaMixin
 
@@ -23,3 +24,15 @@ class ArchiveMetaMixin(type):
 
 class CustomMeta(ArchiveMetaMixin, BindMetaMixin, DeclarativeMeta):
     pass
+
+
+naming_convention = {
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s",
+}
+
+
+custom_metadata = MetaData(naming_convention=naming_convention)
