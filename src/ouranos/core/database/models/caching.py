a8800dfd91a785917354b4e026c0f9d88019b843
Valentin Ambroise <valentin.ambroise@outlook.com>
2024-08-25 19:10:26 +0200

    Add `CachedCRUDMixin`, a class derived from `CRUDMixin` that caches the results from its "non-multiple" CRUD methods

--- src/ouranos/core/database/models/caching.py
+++ src/ouranos/core/database/models/caching.py
@@ -1,14 +1,19 @@
+from __future__ import annotations
+
 import asyncio
+from enum import Enum
 import functools
 from contextlib import AbstractContextManager
-from typing import Any, Callable, MutableMapping, Optional, Type, TypeVar
+from typing import Any, Callable, MutableMapping, Optional, Self, Type, TypeVar
+from uuid import UUID
 
 from cachetools import keys
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from ouranos.core.database.models.abc import Base
+from ouranos.core.database.models.abc import Base, CRUDMixin
 
 
+lookup_keys_type: str | Enum | UUID | bool
 _KT = TypeVar("_KT")
 
 
@@ -91,6 +96,57 @@ def cached(
     return decorator
 
 
+def cached_method(
+    key: Callable[..., _KT] = keys.hashkey,
+    lock: Optional[AbstractContextManager[Any]] = None,
+) :
+    """Decorator to wrap a class method with a memoizing callable that saves
+    results in a cache.
+
+    A mix from cachetools and asyncache
+    """
+    lock = lock or NullContext()
+
+    def decorator(method):
+        if asyncio.iscoroutinefunction(method):
+
+            async def wrapper(cls, *args, **kwargs):
+                k = key(cls, *args, **kwargs)
+                try:
+                    async with lock:
+                        return cls._cache[k]
+                except KeyError:
+                    pass  # key not found
+                v = await method(cls, *args, **kwargs)
+                try:
+                    async with lock:
+                        cls._cache.setdefault(k, v)
+                except ValueError:
+                    pass  # value too large
+                return v
+
+        else:
+
+            def wrapper(cls, *args, **kwargs):
+                k = key(cls, *args, **kwargs)
+                try:
+                    with lock:
+                        return cls._cache[k]
+                except KeyError:
+                    pass  # key not found
+                v = cls._cache(cls, *args, **kwargs)
+                try:
+                    with lock:
+                        cls._cache.setdefault(k, v)
+                except ValueError:
+                    pass  # value too large
+                return v
+
+        return functools.update_wrapper(wrapper, method)
+
+    return decorator
+
+
 def clearer(
     cache: Optional[MutableMapping[_KT, Any]],
     key: Callable[..., _KT] = keys.hashkey,
@@ -124,6 +180,38 @@ def clearer(
         wrapper.cache_lock = lock
 
         return functools.update_wrapper(wrapper, func)
+
+    return decorator
+
+
+def clearing_method(
+    key: Callable[..., _KT] = keys.hashkey,
+    lock: Optional[AbstractContextManager[Any]] = None,
+) :
+    """Decorator to wrap a method to clear some cache results.
+    """
+    lock = lock or NullContext()
+
+    def decorator(method):
+        if asyncio.iscoroutinefunction(method):
+
+            async def wrapper(cls, *args, **kwargs):
+                k = key(cls, *args, **kwargs)
+                v = await method(cls, *args, **kwargs)
+                async with lock:
+                    cls._cache.pop(k, None)
+                return v
+
+        else:
+
+            def wrapper(cls, *args, **kwargs):
+                k = key(cls, *args, **kwargs)
+                v = method(cls, *args, **kwargs)
+                with lock:
+                    cls._cache.pop(k, None)
+                return v
+
+        return functools.update_wrapper(wrapper, method)
 
     return decorator
 
@@ -181,3 +269,49 @@ def clearer_hash(
         **lookup_keys,
 ) -> tuple:
     return create_hashable_key(**lookup_keys)
+
+
+class CachedCRUDMixin(CRUDMixin):
+    _cache: MutableMapping
+
+    @classmethod
+    @clearing_method(key=clearer_hash)
+    async def create(
+            cls,
+            session: AsyncSession,
+            /,
+            values: dict,
+            **lookup_keys: lookup_keys_type,
+    ) -> Self | None:
+        return await super().create(session, values=values, **lookup_keys)
+
+    @classmethod
+    @cached_method(key=cached_hash)
+    async def get(
+            cls,
+            session: AsyncSession,
+            /,
+            **lookup_keys: lookup_keys_type,
+    ) -> Self | None:
+        return await super().get(session, **lookup_keys)
+
+    @classmethod
+    @clearing_method(key=clearer_hash)
+    async def update(
+            cls,
+            session: AsyncSession,
+            /,
+            values: dict,
+            **lookup_keys: lookup_keys_type,
+    ) -> None:
+        return await super().update(session, values=values, **lookup_keys)
+
+    @classmethod
+    @clearing_method(key=clearer_hash)
+    async def delete(
+            cls,
+            session: AsyncSession,
+            /,
+            **lookup_keys: lookup_keys_type,
+    ) -> None:
+        return await super().delete(session, **lookup_keys)
