b40efd2c6aad5a62ecad8f4b8687ff542cfdca07
Valentin Ambroise <valentin.ambroise@outlook.com>
2024-08-26 23:17:21 +0200

    In the tests, clear up the caches when re-creating a db

--- tests/conftest.py
+++ tests/conftest.py
@@ -40,7 +40,12 @@ def config(tmp_path_factory):
 @pytest_asyncio.fixture(scope="module", autouse=True)
 async def db(config: ConfigDict):
     from ouranos.core.database import models  # noqa
+    from ouranos.core.database.models import caches
     await _db.create_all()
     await create_base_data()
     yield _db
     await _db.drop_all()
+    # Clear up the caches
+    for key, value in caches.__dict__.items():
+        if key.startswith("cache_"):
+            value.clear()
