45fa939bd9be70b20d1028e216ab49e0775b2623
Valentin Ambroise <113367796+vaamb@users.noreply.github.com>
2024-10-03 20:21:00 +0200

    Add a Socket.IO event to join and leave rooms (#108)
    * Add a Socket.IO event to join and leave rooms
    
    * Add acknowledgment messages

--- src/ouranos/web_server/events/__init__.py
+++ src/ouranos/web_server/events/__init__.py
@@ -71,6 +71,44 @@ class ClientEvents(AsyncNamespace):
             room=sid
         )
 
+    async def on_join_room(self, sid, room_name: str) -> None:
+        if room_name == ADMIN_ROOM:
+            await self.emit(
+                "join_room_ack",
+                data={
+                    "result": gv.Result.failure,
+                    "reason": "Admin room can only be entered via `login` event."
+                },
+                namespace="/",
+                room=sid
+            )
+        self.server.enter_room(sid, room_name)
+        await self.emit(
+            "join_room_ack",
+            data={"result": gv.Result.success,},
+            namespace="/",
+            room=sid
+        )
+
+    async def on_leave_room(self, sid, room_name: str) -> None:
+        if room_name == ADMIN_ROOM:
+            await self.emit(
+                "leave_room_ack",
+                data={
+                    "result": gv.Result.failure,
+                    "reason": "Admin room can only be left via `login` event."
+                },
+                namespace="/",
+                room=sid
+            )
+        self.server.leave_room(sid, room_name)
+        await self.emit(
+            "leave_room_ack",
+            data={"result": gv.Result.success,},
+            namespace="/",
+            room=sid
+        )
+
     # ---------------------------------------------------------------------------
     #   Events Web clients ->  Web server -> Aggregator
     # ---------------------------------------------------------------------------
