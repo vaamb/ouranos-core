8879b6bfdcd85dd86649f8227d7e1bb03b24e57e
Valentin Ambroise <valentin.ambroise@outlook.com>
2024-10-28 21:23:40 +0100

    Add `PathType`, a `Path` compatible `TypeDecorator`

--- src/ouranos/core/database/models/types.py
+++ src/ouranos/core/database/models/types.py
@@ -1,6 +1,8 @@
 from datetime import datetime, timezone
+from pathlib import Path
 
-from sqlalchemy.types import DateTime, TypeDecorator
+from anyio import Path as ioPath
+from sqlalchemy.types import DateTime, String, TypeDecorator
 
 
 class UtcDateTime(TypeDecorator):
@@ -10,7 +12,24 @@ class UtcDateTime(TypeDecorator):
     def process_bind_param(self, value, dialect):
         if isinstance(value, datetime):
             return value.astimezone(timezone.utc)
+        return value
 
     def process_result_value(self, value, dialect):
         if isinstance(value, datetime):
             return value.replace(tzinfo=timezone.utc)
+        return value
+
+
+class PathType(TypeDecorator):
+    impl = String
+    cache_ok = True
+
+    def process_bind_param(self, value, dialect):
+        if isinstance(value, (ioPath, Path)):
+            return str(value)
+        return value
+
+    def process_result_value(self, value, dialect):
+        if value is not None:
+            return ioPath(value)
+        return value
