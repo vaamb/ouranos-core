652902454db3e728388d4f8e2eaa89a2097eef3e
Valentin Ambroise <valentin.ambroise@outlook.com>
2024-11-09 19:35:08 +0100

    Fix weather routes

--- src/ouranos/web_server/routes/services/weather.py
+++ src/ouranos/web_server/routes/services/weather.py
@@ -20,9 +20,16 @@ router = APIRouter(
 sky_watcher_cache = CacheFactory.get("sky_watcher")
 
 
+# TODO: fix this, it should not be needed
+async def init_cache_if_needed():
+    if not sky_watcher_cache.is_init:
+        await sky_watcher_cache.init()
+
+
 @router.get("/sun_times", response_model=SunTimesResponse)
 async def get_sun_times():
-    response = sky_watcher_cache.get("sun_times")
+    await init_cache_if_needed()
+    response = await sky_watcher_cache.get("sun_times", None)
     if response:
         return response
     return Response(status_code=204)
@@ -30,7 +37,8 @@ async def get_sun_times():
 
 @router.get("/forecast/currently", response_model=CurrentWeatherResponse)
 async def get_current_forecast():
-    response = sky_watcher_cache.get("weather_currently")
+    await init_cache_if_needed()
+    response = await sky_watcher_cache.get("weather_currently", None)
     if response:
         return response
     return Response(status_code=204)
@@ -38,7 +46,8 @@ async def get_current_forecast():
 
 @router.get("/forecast/hourly", response_model=list[HourlyWeatherResponse])
 async def get_current_forecast():
-    response = sky_watcher_cache.get("weather_hourly")
+    await init_cache_if_needed()
+    response = await sky_watcher_cache.get("weather_hourly", None)
     if response:
         return response
     return Response(status_code=204)
@@ -46,7 +55,8 @@ async def get_current_forecast():
 
 @router.get("/forecast/daily", response_model=list[DailyWeatherResponse])
 async def get_current_forecast():
-    response = sky_watcher_cache.get("weather_daily")
+    await init_cache_if_needed()
+    response = await sky_watcher_cache.get("weather_daily", None)
     if response:
         return response
     return Response(status_code=204)
