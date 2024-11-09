5becaa588e245696e7fbf5856ae327a127dcad5b
Valentin Ambroise <113367796+vaamb@users.noreply.github.com>
2024-11-09 19:37:07 +0100

    Add `aioCache`, a sqlite3-backed cache to share data between processes (#126)
    * Add `aioCache`, a sqlite3-backed cache to store weather and un times data
    
    * Add `CacheFactory`, a helper class to get `aioCache`
    
    * Add `aioCache.clear()` method
    
    * Use `aioCache` to replace `DataCache` to store weather and sun times data
    
    * Add a default return option to `aioCache.get()` method
    
    * Nits
    
    * `aiofiles` hasn't been removed in this branch yet
    
    * Fix weather routes

---
+++ src/ouranos/core/caches.py
@@ -0,0 +1,124 @@
+from __future__ import annotations
+
+from pathlib import Path
+import pickle
+from typing import Any, Iterable
+
+import aiosqlite
+
+from ouranos import current_app
+
+_create_query = """
+  CREATE TABLE IF NOT EXISTS %(table_name)s (
+    key TEXT UNIQUE NOT NULL,
+    value BLOB NOT NULL,
+    ttl TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  )
+"""
+_get_query = "SELECT value FROM %(table_name)s WHERE key = CAST(? AS TEXT)"
+_set_query = "REPLACE INTO %(table_name)s (key, value) VALUES (CAST(? AS TEXT), CAST(? AS BLOB))"
+_delete_query = "DELETE FROM %(table_name)s WHERE key = CAST(? AS TEXT)"
+_clear_query = "DELETE FROM %(table_name)s"
+_iter_query = "SELECT key FROM %(table_name)s"
+
+
+class NoDefault:
+    pass
+
+
+no_default = NoDefault()
+
+
+class aioCache:
+    def __init__(self, file_path: Path | str, table: str = "cache") -> None:
+        self._path = Path(file_path)
+        self._table = table
+        self._init: bool = False
+
+    @property
+    def is_init(self) -> bool:
+        return self._init
+
+    def _check_init(self) -> None:
+        if not self.is_init:
+            raise RuntimeError("Cache is not initialized")
+
+    async def init(self):
+        async with aiosqlite.connect(self._path) as db:
+            await db.execute(_create_query % {"table_name": self._table})
+            await db.commit()
+        self._init = True
+
+    async def get(self, key: str, default: Any = no_default) -> Any:
+        self._check_init()
+        async with aiosqlite.connect(self._path) as db:
+            cursor = await db.execute(_get_query % {"table_name": self._table}, (key,))
+            row = await cursor.fetchone()
+        if not row:
+            if default == no_default:
+                raise KeyError(key)
+            return default
+        return pickle.loads(row[0])
+
+    async def set(self, key: str, value: Any) -> None:
+        # Set is inefficient
+        self._check_init()
+        value = pickle.dumps(value)
+        async with aiosqlite.connect(self._path) as db:
+            await db.execute(_set_query % {"table_name": self._table}, (key, value))
+            await db.commit()
+
+    async def delete(self, key: str) -> None:
+        self._check_init()
+        async with aiosqlite.connect(self._path) as db:
+            cursor = await db.execute(_delete_query % {"table_name": self._table}, (key,))
+            if not cursor.rowcount:
+                raise KeyError(key)
+            await db.commit()
+
+    async def clear(self) -> None:
+        self._check_init()
+        async with aiosqlite.connect(self._path) as db:
+            await db.execute(_clear_query % {"table_name": self._table})
+            await db.commit()
+
+    async def keys(self) -> Iterable[str]:
+        self._check_init()
+        async with aiosqlite.connect(self._path) as db:
+            cursor = await db.execute(_iter_query % {"table_name": self._table})
+            rows = await cursor.fetchall()
+            for row in rows:
+                yield row[0]
+
+
+class CacheFactory:
+    __caches: dict[str, aioCache] = {}
+    __config: dict[str, Any] = {
+        "sky_watcher": {
+            "cache_path": "sky_watcher.sqlite",
+        },
+    }
+
+    @classmethod
+    def update_config(cls, cfg: dict[str, Any]) -> None:
+        cls.__config.update(cfg)
+
+    @classmethod
+    def get(
+            cls,
+            name: str,
+    ) -> aioCache:
+        try:
+            return cls.__caches[name]
+        except KeyError:
+            try:
+                cache_path_str = cls.__config[name]["cache_path"]
+            except KeyError:
+                raise ValueError(f"Cache '{name}' does not have a valid config")
+            cache_path = current_app.cache_dir / cache_path_str
+            table_name = cls.__config[name].get("table_name", "cache")
+
+
+            cache = aioCache(cache_path, table_name)
+            cls.__caches[name] = cache
+            return cache
--- src/ouranos/web_server/routes/services/weather.py
+++ src/ouranos/web_server/routes/services/weather.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from fastapi import APIRouter, Response
 
-from ouranos.core.cache import SunTimesCache, WeatherCache
+from ouranos.core.caches import CacheFactory
 from ouranos.web_server.validate.weather import (
     CurrentWeatherResponse, DailyWeatherResponse, HourlyWeatherResponse,
     SunTimesResponse)
@@ -17,10 +17,19 @@ router = APIRouter(
     tags=["app/services/weather"],
 )
 
+sky_watcher_cache = CacheFactory.get("sky_watcher")
+
+
+# TODO: fix this, it should not be needed
+async def init_cache_if_needed():
+    if not sky_watcher_cache.is_init:
+        await sky_watcher_cache.init()
+
 
 @router.get("/sun_times", response_model=SunTimesResponse)
 async def get_sun_times():
-    response = SunTimesCache.get()
+    await init_cache_if_needed()
+    response = await sky_watcher_cache.get("sun_times", None)
     if response:
         return response
     return Response(status_code=204)
@@ -28,7 +37,8 @@ async def get_sun_times():
 
 @router.get("/forecast/currently", response_model=CurrentWeatherResponse)
 async def get_current_forecast():
-    response = WeatherCache.get_currently()
+    await init_cache_if_needed()
+    response = await sky_watcher_cache.get("weather_currently", None)
     if response:
         return response
     return Response(status_code=204)
@@ -36,7 +46,8 @@ async def get_current_forecast():
 
 @router.get("/forecast/hourly", response_model=list[HourlyWeatherResponse])
 async def get_current_forecast():
-    response = WeatherCache.get_hourly()
+    await init_cache_if_needed()
+    response = await sky_watcher_cache.get("weather_hourly", None)
     if response:
         return response
     return Response(status_code=204)
@@ -44,7 +55,8 @@ async def get_current_forecast():
 
 @router.get("/forecast/daily", response_model=list[DailyWeatherResponse])
 async def get_current_forecast():
-    response = WeatherCache.get_daily()
+    await init_cache_if_needed()
+    response = await sky_watcher_cache.get("weather_daily", None)
     if response:
         return response
     return Response(status_code=204)
