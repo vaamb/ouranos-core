10dbae06b99829653f273d45fc67910119f3b0ba
Valentin Ambroise <valentin.ambroise@outlook.com>
2024-11-25 23:48:04 +0100

    Reduce the code duplication in routes dispatching CRUD events

--- src/ouranos/web_server/routes/gaia/utils.py
+++ src/ouranos/web_server/routes/gaia/utils.py
@@ -1,7 +1,13 @@
+from typing import Any
+
 from fastapi import HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from dispatcher import AsyncDispatcher
+import gaia_validators as gv
+
 from ouranos.core.database.models.gaia import Ecosystem
+from ouranos.core.dispatchers import DispatcherFactory
 
 
 eids_desc = (
@@ -25,4 +31,26 @@ async def ecosystem_or_abort(
     raise HTTPException(
         status_code=status.HTTP_404_NOT_FOUND,
         detail="No ecosystem(s) found"
+    )
+
+
+async def emit_crud_event(
+        ecosystem: Ecosystem,
+        action: gv.CrudAction,
+        target: str,
+        data: Any
+) -> None:
+    dispatcher: AsyncDispatcher = DispatcherFactory.get("application-internal")
+    await dispatcher.emit(
+        event="crud",
+        data=gv.CrudPayload(
+            routing=gv.Route(
+                engine_uid=ecosystem.engine_uid,
+                ecosystem_uid=ecosystem.uid
+            ),
+            action=action,
+            target=target,
+            data=data,
+        ).model_dump(),
+        namespace="aggregator-internal",
     )
