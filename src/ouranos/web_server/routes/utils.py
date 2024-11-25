8ab1c17ecdfaf7b8f02cd298d162f1a1c6f10ac1
Valentin Ambroise <valentin.ambroise@outlook.com>
2024-11-26 00:27:22 +0100

    Still more nits

--- src/ouranos/web_server/routes/utils.py
+++
@@ -1,9 +0,0 @@
-from fastapi import HTTPException, status
-
-
-def assert_single_uid(uid: str, name: str = "uid"):
-    if len(uid.split(",")) > 1:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"`{name}` should be a single valid {name}"
-        )
