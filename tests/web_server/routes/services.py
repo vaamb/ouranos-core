aa267bc9d267e7628c244c681403752770e9fe68
Valentin Ambroise <valentin.ambroise@outlook.com>
2024-11-27 21:26:38 +0100

    remove debugging remnants

--- tests/web_server/routes/services.py
+++ tests/web_server/routes/services.py
@@ -1,10 +1,6 @@
 from fastapi.testclient import TestClient
 
-from ouranos.core.database.models.app import Service
 
-import pytest
-
-@pytest.mark.asyncio
 async def test_route_protection(client: TestClient, db):
     response = client.get("/api/app/services/weather/sun_times")
     assert response.status_code in (200, 204)
