ae058fa2bdee2785c91f58b8e60f1bb686a823f1
Valentin Ambroise <valentin.ambroise@outlook.com>
2024-11-28 19:09:26 +0100

    Bump to version 0.8.0
    * Breaking changes:
    
    - Change the routes related to hardware, sensors and camera images
    
    - Drop support of Python 3.9 and 3.10
    
    * Major changes:
    
    - Improve caching of DB queries
    
    - Add Socket.IO events allowing the client to join and leave rooms
    
    - Add a GitHub action on push and pull requests
    
    - Add a server for Gaia to upload camera images
    
    - Fix the `SkyWatcher`
    
    * Minor changes
    
    - Fuse back `StreamGaiaEvents` into `GaiaEvents`

--- src/ouranos/__init__.py
+++ src/ouranos/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.7.1"
+__version__ = "0.8.0"
 
 from ouranos.core.globals import current_app, db, scheduler
 from ouranos.core.config import app_info, configure_logging, setup_config
