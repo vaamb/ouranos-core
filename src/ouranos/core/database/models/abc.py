fc8b50d3270041c38a1164d85b2dc8cb28e1f3f6
Valentin Ambroise <valentin.ambroise@outlook.com>
2025-02-01 12:52:08 +0100

    Add `offset`, `limit` and `order_by` parameters to `CRUDMixin.get()` method

--- src/ouranos/core/database/models/abc.py
+++ src/ouranos/core/database/models/abc.py
@@ -6,7 +6,8 @@ from enum import Enum
 from typing import NamedTuple, Self, Sequence
 from uuid import UUID
 
-from sqlalchemy import and_, delete, insert, inspect, Select, select, update
+from sqlalchemy import (
+    and_, delete, insert, inspect, Select, select, UnaryExpression, update)
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from gaia_validators import missing
@@ -100,9 +101,15 @@ class CRUDMixin:
             cls,
             session: AsyncSession,
             /,
-            **lookup_keys: lookup_keys_type | None,
+            offset: int | None = None,
+            limit: int | None = None,
+            order_by: UnaryExpression | None = None,
+            **lookup_keys: list[lookup_keys_type] | lookup_keys_type | None,
     ) -> Self | None:
         """
+        :param offset: the offset from which to start looking
+        :param limit: the maximum number of rows to query
+        :param order_by: how to order the results
         :param session: an AsyncSession instance
         :param lookup_keys: a dict with table column names as keys and values
                             depending on the related column data type
@@ -118,7 +125,7 @@ class CRUDMixin:
             /,
             offset: int | None = None,
             limit: int | None = None,
-            order_by: str | None = None,
+            order_by: UnaryExpression | None = None,
             **lookup_keys: list[lookup_keys_type] | lookup_keys_type | None,
     ) -> Sequence[Self]:
         """
