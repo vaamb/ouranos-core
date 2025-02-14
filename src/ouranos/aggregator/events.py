983a796e101bb368e6ba76d8e50a448364d03296
Valentin Ambroise <valentin.ambroise@outlook.com>
2025-02-14 21:43:47 +0100

    Set camera hardware level to "ecosystem"

--- src/ouranos/aggregator/events.py
+++ src/ouranos/aggregator/events.py
@@ -595,6 +595,8 @@ class GaiaEvents(AsyncEventHandler):
                     hardware["in_config"] = True  # noqa
                     # TODO: register multiplexer ?
                     del hardware["multiplexer_model"]  # noqa
+                    if hardware["type"] == gv.HardwareType.camera:
+                        hardware["level"] = gv.HardwareLevel.ecosystem
                     await Hardware.update_or_create(
                         session, uid=hardware_uid, values=hardware)
 
