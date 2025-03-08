9caaf808f57b3f600f94d76172ab6d3836564ce6
Valentin Ambroise <valentin.ambroise@outlook.com>
2025-03-08 10:23:45 +0100

    Add a function to get html body text

--- src/ouranos/core/email/templates/__init__.py
+++ src/ouranos/core/email/templates/__init__.py
@@ -1,8 +1,11 @@
+from html.parser import HTMLParser
 from pathlib import Path
+import re
 
 from jinja2 import Environment, FileSystemLoader
 
 
+# Template rendering
 template_folder = Path(__file__).absolute().parent
 
 loader = FileSystemLoader(template_folder)
@@ -13,3 +16,21 @@ environment = Environment(
 async def render_template(rel_path, **context) -> str:
     template = await environment.get_template(f"{rel_path}.html").render_async(context)
     return template
+
+
+# Template body linearization
+body_pattern = re.compile(r'<body>((.|\n)*?)</body>', re.RegexFlag.MULTILINE)
+
+class HTMLFilter(HTMLParser):
+    text = ""
+    def handle_data(self, data):
+        self.text += data
+
+
+def get_body_text(template: str) -> str:
+    body = body_pattern.search(template)
+    if not body:
+        return ""
+    filter = HTMLFilter()
+    filter.feed(body.group(1))
+    return filter.text.strip()
