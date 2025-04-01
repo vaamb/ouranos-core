733a069482a82f57a4518bc4d7f24269f619e0f1
Valentin Ambroise <113367796+vaamb@users.noreply.github.com>
2025-04-02 00:31:13 +0200

    Remove unused consts (#201)

--- src/ouranos/core/config/consts.py
+++ src/ouranos/core/config/consts.py
@@ -2,41 +2,9 @@ from datetime import datetime, timezone
 from enum import Enum
 
 
-class ImmutableDict(dict):
-    def __setitem__(self, key, value):
-        raise AttributeError
-
-
 START_TIME = datetime.now(timezone.utc).replace(microsecond=0)
 
-WEATHER_MEASURES = ImmutableDict({
-    "mean": ["temperature", "temperatureLow", "temperatureHigh", "humidity",
-             "windSpeed", "cloudCover", "precipProbability", "dewPoint"],
-    "mode": ["summary", "icon", "sunriseTime", "sunsetTime"],
-    "other": ["time", "sunriseTime", "sunsetTime"],
-    "range": ["temperature"],
-})
-
-WEATHER_DATA_MULTIPLICATION_FACTORS = ImmutableDict({
-    "temperature": 1,
-    "humidity": 100,
-    "windSpeed": 1,
-    "cloudCover": 100,
-    "precipProbability": 100,
-})
-
 ECOSYSTEM_TIMEOUT = 60
-
-ACTUATORS_AVAILABLE = ["gpioDimmable", "gpioSwitch"]
-
-PHYSICAL_SENSORS_AVAILABLE = ["DHT11", "DHT22", "VEML7700"]
-VIRTUAL_SENSORS_AVAILABLE = [
-    "virtualDHT11", "virtualDHT22", "virtualMega", "virtualMoisture",
-    "virtualVEML7700"
-]
-SENSORS_AVAILABLE = PHYSICAL_SENSORS_AVAILABLE + VIRTUAL_SENSORS_AVAILABLE
-
-HARDWARE_AVAILABLE = ACTUATORS_AVAILABLE + SENSORS_AVAILABLE
 
 
 # File size
