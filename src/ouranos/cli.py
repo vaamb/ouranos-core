7078dabca519d8e56038241b864e83656a7c82a1
Valentin Ambroise <valentin.ambroise@outlook.com>
2025-05-20 20:22:15 +0200

    Update the cli

--- src/ouranos/cli.py
+++ src/ouranos/cli.py
@@ -4,22 +4,11 @@ from typing import Any
 
 from click import Command, Context, MultiCommand
 
-from ouranos.aggregator.main import main as aggregator
 from ouranos.core.config import ConfigHelper
 from ouranos.core.plugins_manager import PluginManager
-from ouranos.web_server.main import main as webserver
 
 
 class RootCommand(MultiCommand):
-    _builtin_commands = {
-        "aggregator": aggregator,
-        "server": webserver
-    }
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self._plugin_commands: dict[str, Command] = {}
-
     def _get_config_profile(self, ctx: Context) -> Any:
         default_profile = None
         for param in self.params:
@@ -28,17 +17,6 @@ class RootCommand(MultiCommand):
                 break
         return ctx.params.get("config_profile", default_profile)
 
-    @property
-    def plugin_commands(self) -> dict[str, Command]:
-        if not ConfigHelper.config_is_set():
-            raise RuntimeError("Config should be set to use this method.")
-        if not self._plugin_commands:
-            pm: PluginManager = PluginManager()
-            pm.register_plugins()
-            for plugin_name, plugin in pm.plugins.items():
-                self._plugin_commands[plugin_name] = plugin.command
-        return self._plugin_commands
-
     def get_command(self, ctx: Context, cmd_name: str) -> Command | None:
         if not ConfigHelper.config_is_set():
             # When using "--help" flag, click will loop through all the commands
@@ -46,13 +24,16 @@ class RootCommand(MultiCommand):
             #  to get the info needed.
             config_profile = self._get_config_profile(ctx)
             ConfigHelper.set_config_and_configure_logging(config_profile)
-        if cmd_name in self._builtin_commands:
-            return self._builtin_commands[cmd_name]
-        return self.plugin_commands.get(cmd_name)
+        pm: PluginManager = PluginManager()
+        pm.register_plugins()
+        plugin = pm.plugins.get(cmd_name)
+        if plugin is None:
+            return None
+        return plugin.command
 
     def list_commands(self, ctx: Context) -> list[str]:
         config_profile = self._get_config_profile(ctx)
         ConfigHelper.set_config_and_configure_logging(config_profile)
-        plugin_commands_list = [cmd for cmd in self.plugin_commands]
-        plugin_commands_list.sort()
-        return [cmd for cmd in self._builtin_commands] + plugin_commands_list
+        pm: PluginManager = PluginManager()
+        pm.register_plugins()
+        return [plugin_name for plugin_name in pm.plugins.keys()]
