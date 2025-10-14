9970c8cbf51ba2d1c138a4e8ceacb3063cd1c3c2
Valentin Ambroise <113367796+vaamb@users.noreply.github.com>
2025-10-14 22:24:17 +0200

    Bump to version 0.10.0 (#296)
    * Major changes
    
    - Pin personal sub-dependencies
    
    - Improve installation and update scripts, allow ouranos plugin packages to have their own update script
    
    - Add a "foreground" option to `start.sh`
    
    - Allow to launch omitted plugins from the CLI
    
    - Remove the deprecated "environmental_parameters" event
    
    - Improve `Functionality` and `Plugin` to gracefully exit, use the `{startup,shutdown}` naming, add docstring
    
    - Log to file by default
    
    - Add a QC workflow
    
    * Minor changes
    
    - Various fixes
    
    - Add tests

--- src/ouranos/__init__.py
+++ src/ouranos/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.9.0"
+__version__ = "0.10.0"
 
 from ouranos.core.globals import current_app, db, scheduler
 from ouranos.core.config import app_info, configure_logging, setup_config
