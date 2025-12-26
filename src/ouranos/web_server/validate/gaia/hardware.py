a045a0c0f162f018cd4b3f0647c009b7cfab911e
Valentin Ambroise <valentin.ambroise@outlook.com>
2025-12-26 15:59:27 +0100

    Bump `gaia-validators`

--- src/ouranos/web_server/validate/gaia/hardware.py
+++ src/ouranos/web_server/validate/gaia/hardware.py
@@ -30,31 +30,72 @@ class HardwareType(BaseModel):
     value: int
 
 
-class HardwareUpdatePayload(gv.AnonymousHardwareConfig):
-    model_config = ConfigDict(
-        extra="ignore",
-    )
-
+class HardwareUpdatePayload(BaseModel):
     name: str | MissingValue = missing
     active: bool | MissingValue = missing
-    level: gv.HardwareLevel | MissingValue = missing
     address: str | MissingValue = missing
     type: gv.HardwareType | MissingValue = missing
+    level: gv.HardwareLevel | MissingValue = missing
+    groups: set[str] | MissingValue = missing
     model: str | MissingValue = missing
-    status: bool | MissingValue = missing
     measures: list[str] | MissingValue = missing
-    plant_uid: list[str] | MissingValue = missing
-
-    @field_validator("level", mode="before")
-    def parse_level(cls, value):
-        if isinstance(value, str):
-            return safe_enum_from_name(gv.HardwareLevel, value)
-        return value
+    plants: list[str] | MissingValue = missing
+    multiplexer_model: str | MissingValue = missing
 
     @field_validator("type", mode="before")
+    @classmethod
     def parse_type(cls, value):
+        if value == missing:
+            return value
+        if isinstance(value, int):
+            return gv.HardwareType(value)
+        return safe_enum_from_name(gv.HardwareType, value)
+
+    @field_validator("level", mode="before")
+    @classmethod
+    def parse_level(cls, value):
+        if value == missing:
+            return value
+        return safe_enum_from_name(gv.HardwareLevel, value)
+
+    @field_validator("groups", mode="before")
+    @classmethod
+    def parse_groups(cls, value: str | list[str]):
+        if value == missing:
+            return value
         if isinstance(value, str):
-            return safe_enum_from_name(gv.HardwareType, value)
+            return {value}
+        return set(value)
+
+    @field_validator("measures", mode="before")
+    @classmethod
+    def parse_measures(cls, value: str | list[str] | list[dict[str, str | None]] | None):
+        if value == missing:
+            return value
+        if value is None:
+            return []
+        if isinstance(value, str):
+            value = [value]
+        rv = []
+        for v in value:
+            if isinstance(v, str):
+                v_split = v.split("|")
+                name = v_split[0]
+                unit = v_split[1] if len(v_split) > 1 else None
+                rv.append({"name": name, "unit": unit})
+            else:
+                rv.append(v)
+        return rv
+
+    @field_validator("plants", mode="before")
+    @classmethod
+    def parse_plants(cls, value: str | list[str] | None):
+        if value == missing:
+            return value
+        if value is None:
+            return []
+        if isinstance(value, str):
+            return [value]
         return value
 
 
