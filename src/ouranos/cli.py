1d74fb4bbfef40430e15776439600180de1c44cc
Valentin Ambroise <113367796+vaamb@users.noreply.github.com>
2025-12-28 23:19:04 +0100

    Add a CLI command to fill the DB with the default tables (#332)
    * Use `click.Group` for `RootCommand` and allow to add new commands through `add_command`
    
    * Nits
    
    * Add the "fill-db" CLI command
    
    * Naming nits
    
    * Nits in `Plugin` naming
    
    * Nits in command ordering
    
    * Reduce diff
    
    * Use flag

--- src/ouranos/cli.py
+++ src/ouranos/cli.py
@@ -1,14 +1,15 @@
 from __future__ import annotations
 
 from typing import Any
+import warnings
 
-from click import Command, Context, MultiCommand
+from click import Command, Context, Group
 
 from ouranos.core.config import ConfigHelper
 from ouranos.core.plugins_manager import PluginManager
 
 
-class RootCommand(MultiCommand):
+class RootCommand(Group):
     def _get_config_profile(self, ctx: Context) -> Any:
         default_profile = None
         for param in self.params:
@@ -17,23 +18,32 @@ class RootCommand(MultiCommand):
                 break
         return ctx.params.get("config_profile", default_profile)
 
-    def get_command(self, ctx: Context, cmd_name: str) -> Command | None:
+    def _set_config(self, ctx: Context) -> None:
         if not ConfigHelper.config_is_set():
-            # When using "--help" flag, click will loop through all the commands
-            #  found via `list_commands` and use `get_command` on each of them
-            #  to get the info needed.
             config_profile = self._get_config_profile(ctx)
-            ConfigHelper.set_config_and_configure_logging(config_profile)
+            with warnings.catch_warnings():
+                warnings.simplefilter("ignore")
+                ConfigHelper.set_config_and_configure_logging(config_profile)
+
+    def get_command(self, ctx: Context, cmd_name: str) -> Command | None:
+        # If the command is a registered command, return it
+        if cmd_name in self.commands:
+            return self.commands[cmd_name]
+
+        # If the command is a plugin command, return it
+        self._set_config(ctx)
         pm: PluginManager = PluginManager()
-        pm.register_plugins(omit_excluded=False)  # Allow to launch an omitted plugin
-        plugin = pm.plugins.get(cmd_name)
-        if plugin is None:
-            return None
-        return plugin.command
+        if not pm.plugins:
+            pm.register_plugins(omit_excluded=False)  # Allow to launch an omitted plugin
+        if cmd_name in pm.plugins:
+            return pm.plugins[cmd_name].command
+
+        # If the command is not found, return None
+        return None
 
     def list_commands(self, ctx: Context) -> list[str]:
-        config_profile = self._get_config_profile(ctx)
-        ConfigHelper.set_config_and_configure_logging(config_profile)
+        self._set_config(ctx)
         pm: PluginManager = PluginManager()
         pm.register_plugins(omit_excluded=False)  # List all plugins installed
-        return [plugin_name for plugin_name in pm.plugins.keys()]
+        # Plugins are already sorted by the PluginManager
+        return [*pm.plugins.keys(), *sorted(self.commands)]
