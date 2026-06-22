40c2eaa2230c98000f6ef7438ed4d40efaa89fee
Valentin Ambroise <113367796+vaamb@users.noreply.github.com>
2026-06-22 21:47:47 +0200

    Fix an issue that allows regular user to leave "admin" room (#394)
    * Nits
    
    * Fix an issue that allows regular user to leave "admin" room
    
    * Re-enable the associated test

--- src/ouranos/web_server/events/__init__.py
+++ src/ouranos/web_server/events/__init__.py
@@ -24,8 +24,18 @@ logger: Logger = getLogger("aggregator.socketio")
 class ClientEvents(AsyncNamespace):
     def __init__(self, namespace=None):
         super().__init__(namespace=namespace)
-        self.server: AsyncServer | None = None
+        self._server: AsyncServer | None = None
         self._ouranos_dispatcher: AsyncDispatcher | None = None
+
+    @property
+    def server(self) -> AsyncServer:
+        if not self._server:
+            raise RuntimeError("You need to set the Ouranos event dispatcher")
+        return self._server
+
+    @server.setter
+    def server(self, server: AsyncServer):
+        self._server = server
 
     @property
     def ouranos_dispatcher(self) -> AsyncDispatcher:
@@ -125,6 +135,7 @@ class ClientEvents(AsyncNamespace):
                 namespace="/",
                 room=sid
             )
+            return
         await self.server.leave_room(sid, room_name)
         await self.emit(
             "leave_room_ack",
