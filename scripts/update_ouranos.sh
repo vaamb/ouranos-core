7db27d9932f87141194ea295f2619ac8a38f3f04
Valentin Ambroise <valentin.ambroise@outlook.com>
2026-09-05 12:16:12 +0200

    Update the master `pyproject.toml` version when updating ouranos

--- scripts/update_ouranos.sh
+++ scripts/update_ouranos.sh
@@ -227,6 +227,13 @@ update_packages() {
     fi
 
     cd "$OURANOS_DIR"
+
+    # Update the master pyproject version number
+    local ouranos_version
+    ouranos_version=$(python -c "import sys; import ouranos; sys.stdout.write(ouranos.__version__); sys.stdout.flush(); sys.exit(0)")
+    sed -i 's/^version = .*/version = "'"${ouranos_version}"'"/' pyproject.toml
+
+    # Update uv lock and packages
     uv lock --upgrade ||
         die "Failed to update uv lock"
     # use --inexact to keep packages not defined in pyproject.toml such as the DB drivers
